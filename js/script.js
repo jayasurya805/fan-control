bc70c98f8310b8183f86b6309b54420851d1fbee
B.Jaya Surya <167969009+jayasurya805@users.noreply.github.com>
2024-05-22 15:38:46 +0530

    Add files via upload
    The fan control project is a web-based application developed using HTML, CSS, and JavaScript. The purpose of this project is to provide users with a simple interface to control the speed or state of a fan device.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let a = document.getElementById("img");
+function myfanon() {
+  a.style.animationDuration = 3 + "s";
+}
+function myfanoff() {
+  a.style.animationDuration = 0 + "s";
+}
+function myfan2() {
+  a.style.animationDuration = 1 + "s";
+}
+function myfan3() {
+  a.style.animationDuration = 0.5 + "s";
+}
+function myfan4() {
+  a.style.animationDuration = 0.1 + "s";
+}
